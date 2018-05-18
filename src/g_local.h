ec8818fd126f25a46b15d33d25115384f2f663c5
earth-metal <38820385+earth-metal@users.noreply.github.com>
2018-05-18 17:14:50 -0400

    Update code-constant for patch version

--- src/g_local.h
+++ src/g_local.h
@@ -17,7 +17,7 @@
 #define ROGUE							//Rogue mission pack 2
 //-------------------------------------------------------------
 
-#define UGLADQ2_VERSION			"0.98.2u"
+#define UGLADQ2_VERSION			"0.98.3u"
 
 #define max(X,Y)    (X > Y ? X : Y)
 #define min(X,Y)    (X < Y ? X : Y)
